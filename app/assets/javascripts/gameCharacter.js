ff894248cd9ff9ab14d762896b1a48bdee893060
Daniel Chu <danchu123@gmail.com>
2016-07-14 19:14:56 -0500

    added gameCharacter

--- app/assets/javascripts/gameCharacter.js
+++ app/assets/javascripts/gameCharacter.js
@@ -9,12 +9,30 @@ function GameCharacter(unitName, unitType, unitHp, unitAttack, alive, isNpc){
   this.act = function(){
 
     if(this.isNpc == false){
-      button.visible = true;
-      //console.log(button.visible);
+      attackButton.visible = true;
+      //console.log(attackButton.visible);
     }
-
     if(this.isNpc == true){
-      button.visble = false;
+      attackButton.visble = false;
+      arrayLength = charsArray.length;
+
+      var charIndex = Math.floor(Math.random() * arrayLength);
+
+      charsArray[charIndex].unitHp = charsArray[charIndex].unitHp - current_unit.unitAttack;
+
+      if(charsArray[charIndex].unitHp < 1){
+        charsArray[charIndex].alive = false;
+        charsArray[charIndex].unitHp = 0;
+        console.log("IT attacks " + charsArray[charIndex].name);
+        charText[charIndex].setText(charsArray[charIndex].name + "\n" + charsArray[charIndex].unitHp);
+        deadChars.push(charsArray.splice(charIndex,1));
+        deadCharText.push(charText.splice(charIndex,1));
+        return;
+      }
+
+      console.log("IT attacks " + charsArray[charIndex].name);
+      charText[charIndex].setText(charsArray[charIndex].name + "\n" + charsArray[charIndex].unitHp);
+      //next_turn();
     }
     
   }
