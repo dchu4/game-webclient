5b7d52b5aea9ebde8126bdff955ff3e9bb9a2c23
Daniel Chu <danchu123@gmail.com>
2016-07-20 14:34:31 -0500

    updated character database, updated battle phase to work with objects from json.

--- app/assets/javascripts/bootState.js
+++ app/assets/javascripts/bootState.js
@@ -16,13 +16,45 @@ var bootState = {
           charObj = {
             name: data[i].character_name,
             charClass: data[i].character_class,
-            charHp: 300,
+            charHp: data[i].hp,
             charAttack: data[i].attack,
-            alive: true,
-            set: data[i].enemy_set
+            alive: data[i].alive,
+            set: data[i].enemy_set,
+            isNpc: data[i].is_npc,
+            act: function(){
+              if(this.isNpc == false){
+                attackButton.visible = true;
+                //console.log(attackButton.visible);
+              }
+              if(this.isNpc == true){
+                attackButton.visble = false;
+
+                arrayLength = userParty.length;
+
+                console.log("This is the length: " + arrayLength);
+
+                var charIndex = Math.floor(Math.random() * arrayLength);
+
+                userParty[charIndex].charHp = userParty[charIndex].charHp - current_unit.charAttack;
+
+                if(userParty[charIndex].charHp < 1){
+                  userParty[charIndex].alive = false;
+                  userParty[charIndex].charHp = 0;
+                  console.log("IT attacks " + userParty[charIndex].name);
+                  charText[charIndex].setText(userParty[charIndex].name + "\n" + "HP: " + userParty[charIndex].charHp);
+                  deadChars.push(userParty.splice(charIndex,1));
+                  deadCharText.push(charText.splice(charIndex,1));
+                  return;
+                }
+
+                console.log("IT attacks " + userParty[charIndex].name);
+                charText[charIndex].setText(userParty[charIndex].name + "\n" + "HP: " + userParty[charIndex].charHp);
+                //next_turn();
+              }
+            }
           }
 
-          if(data[i].isNpc){
+          if(data[i].is_npc){
             enemysArray.push(charObj);
           }
           else{
@@ -42,16 +74,14 @@ var bootState = {
         .then(function(data){
           for(var i = 0; i < data.length; i++){
             itemObj = {
-              itemType: data[i].itemType,
-              itemName: data[i].itemName,
-              cureStatus: data[i].cureStatus,
+              itemType: data[i].item_type,
+              itemName: data[i].name,
+              cureStatus: data[i].cure_status,
               attack: data[i].attack,
               defense: data[i].defense,
-              magicDefense: data[i].magicDefense,
-              magicAttack: data[i].magicAttack,
-              recoveryAmount: data[i].recoveryAmount,
-              isEquipped: data[i].isEquipped,
-              quantity: data[i].quantity,
+              magicDefense: data[i].magic_defense,
+              magicAttack: data[i].magic_attack,
+              recoveryAmount: data[i].recovery_amount,
               itemId: data[i].id
             }
 
